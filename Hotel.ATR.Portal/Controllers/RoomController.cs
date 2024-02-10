51f5b416c798dd1b99da9bdda5ce4501e27e0bd9
Orazbay <you@biy.kz>
2024-02-10 14:29:11 +0600

    Добавьте файлы проекта.

---
+++ Hotel.ATR.Portal/Controllers/RoomController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Hotel.ATR.Portal.Controllers
+{
+    public class RoomController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult RenderPartial()
+        {
+            return View();
+        }
+
+        public IActionResult RoomList()
+        {
+            return View();
+        }
+
+        public IActionResult RoomDetails()
+        {
+            return View();
+        }
+    }
+}
---
+++ Hotel.ATR.Portal/Controllers/RoomListController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Hotel.ATR.Portal.Controllers
+{
+    public class RoomListController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
